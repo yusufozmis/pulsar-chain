fcccaf3050abb88e7599b992dcae7353ac327c01
Yusuf <yusufozmis2@gmail.com>
2026-03-05 23:07:54 +0300

    fix(keyregistry): improve register keys logic

--- x/keyregistry/keeper/msg_server_register_keys.go
+++ x/keyregistry/keeper/msg_server_register_keys.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	errorsmod "cosmossdk.io/errors"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/node101-io/pulsar-chain/x/keyregistry/types"
 )
 
@@ -17,25 +19,52 @@ func VerifyCosmosSig(sig string, msg, cosmosPublicKey []byte) bool {
 	return true
 }
 
+func DeriveAddressFromPubkey(cosmosPublicKey []byte) string {
+	pubKey := secp256k1.PubKey{
+		Key: cosmosPublicKey,
+	}
+	addr := sdk.AccAddress(pubKey.Address())
+	return addr.String()
+}
+
 func (k msgServer) RegisterKeys(ctx context.Context, msg *types.MsgRegisterKeys) (*types.MsgRegisterKeysResponse, error) {
 	if _, err := k.addressCodec.StringToBytes(msg.Creator); err != nil {
-		return nil, errorsmod.Wrap(err, "invalid authority address")
+		return nil, errorsmod.Wrap(types.ErrInvalidCreatorAddres, "")
+	}
+
+	if len(msg.CosmosPublicKey) != 33 {
+		return nil, errorsmod.Wrap(types.ErrInvalidPublicKey, "cosmos pubkey must be compressed (33 bytes)")
+	}
+
+	derivedAddress := DeriveAddressFromPubkey(msg.CosmosPublicKey)
+
+	if derivedAddress != msg.Creator {
+		return nil, errorsmod.Wrap(types.ErrInvalidSigner, "creator does not match provided cosmos public key")
+	}
+
+	cosmosKeyExists, err := k.Keeper.CosmosToMina.Has(ctx, msg.CosmosPublicKey)
+
+	minaKeyExists, err := k.Keeper.MinaToCosmos.Has(ctx, msg.MinaPublicKey)
+
+	if cosmosKeyExists || minaKeyExists {
+		return nil, errorsmod.Wrap(types.ErrSecondaryKeyExists, "")
 	}
 
 	minaSigValidity := VerifyMinaSig(msg.MinaSignature, msg.CosmosPublicKey, msg.MinaPublicKey)
 
 	cosmosSigValidity := VerifyCosmosSig(msg.CosmosSignature, msg.MinaPublicKey, msg.CosmosPublicKey)
 
-	if minaSigValidity && cosmosSigValidity {
+	if !minaSigValidity || !cosmosSigValidity {
+		return nil, errorsmod.Wrap(types.ErrInvalidSignature, "Invalid Cosmos or Mina Signature")
+	}
 
-		err := k.Keeper.CosmosToMina.Set(ctx, msg.CosmosPublicKey, msg.MinaPublicKey)
-		if err != nil {
-			return &types.MsgRegisterKeysResponse{}, err
-		}
-		err = k.Keeper.MinaToCosmos.Set(ctx, msg.MinaPublicKey, msg.CosmosPublicKey)
-		if err != nil {
-			return &types.MsgRegisterKeysResponse{}, err
-		}
+	err = k.Keeper.CosmosToMina.Set(ctx, msg.CosmosPublicKey, msg.MinaPublicKey)
+	if err != nil {
+		return nil, err
+	}
+	err = k.Keeper.MinaToCosmos.Set(ctx, msg.MinaPublicKey, msg.CosmosPublicKey)
+	if err != nil {
+		return nil, err
 	}
 
 	return &types.MsgRegisterKeysResponse{}, nil
--- x/keyregistry/types/errors.go
+++ x/keyregistry/types/errors.go
@@ -8,5 +8,9 @@ import (
 
 // x/keyregistry module sentinel errors
 var (
-	ErrInvalidSigner = errors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrInvalidSigner        = errors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrInvalidSignature     = errors.Register(ModuleName, 1101, "Invalid Signature")
+	ErrSecondaryKeyExists   = errors.Register(ModuleName, 1102, "Secondary key already exists")
+	ErrInvalidCreatorAddres = errors.Register(ModuleName, 1103, "Invalid creator address")
+	ErrInvalidPublicKey     = errors.Register(ModuleName, 1104, "Invalid Public key")
 )
