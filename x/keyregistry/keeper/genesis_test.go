c880eb792adb5349580005e8f5fdbcdec9c8e812
Yusuf <yusufozmis2@gmail.com>
2026-03-09 22:00:27 +0300

    test: add test for init/export genesis

--- x/keyregistry/keeper/genesis_test.go
+++ x/keyregistry/keeper/genesis_test.go
@@ -1,8 +1,11 @@
 package keeper_test
 
 import (
+	"crypto/ed25519"
+	"crypto/rand"
 	"testing"
 
+	"github.com/cometbft/cometbft/crypto/secp256k1"
 	"github.com/node101-io/pulsar-chain/x/keyregistry/types"
 
 	"github.com/stretchr/testify/require"
@@ -21,4 +24,36 @@ func TestGenesis(t *testing.T) {
 	require.NotNil(t, got)
 
 	require.EqualExportedValues(t, genesisState.Params, got.Params)
+}
+func TestInitAndExportGenesis(t *testing.T) {
+
+	f := initFixture(t)
+
+	cosmosPriv := secp256k1.GenPrivKey()
+
+	cosmosPubKey := cosmosPriv.PubKey()
+
+	minaPubKey, _, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		panic(err)
+	}
+
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+		KeyPairs: []*types.KeyPair{
+			{
+				MinaKey:   minaPubKey,
+				CosmosKey: cosmosPubKey.Bytes(),
+			},
+		},
+	}
+
+	err = f.keeper.InitGenesis(f.ctx, genesisState)
+	require.NoError(t, err)
+	got, err := f.keeper.ExportGenesis(f.ctx)
+	require.NoError(t, err)
+	require.NotNil(t, got)
+
+	require.EqualExportedValues(t, genesisState.KeyPairs, got.KeyPairs)
+
 }
