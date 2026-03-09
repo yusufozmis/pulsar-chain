ce06055ad0106c176af23d10f40c67164f6345b1
Yusuf <yusufozmis2@gmail.com>
2026-03-09 17:30:57 +0300

    test: cover invalidSigner and invalidCreatorAddress errors on keyregistry tests

--- x/keyregistry/keeper/register_keys_test.go
+++ x/keyregistry/keeper/register_keys_test.go
@@ -59,6 +59,79 @@ func TestRegisterKeysSuccess(t *testing.T) {
 	require.Equal(t, exists, true)
 
 }
+
+func TestInvalidCreatorAddress(t *testing.T) {
+
+	priv := secp256k1.GenPrivKey()
+
+	pub := priv.PubKey()
+
+	f := initFixture(t)
+	ms := keeper.NewMsgServerImpl(f.keeper)
+
+	_, err := ms.RegisterKeys(f.ctx, &types.MsgRegisterKeys{
+		Creator:         "creator",
+		CosmosSignature: mockCosmosSignature,
+		MinaSignature:   mockMinaSignature,
+		CosmosPublicKey: pub.Bytes(),
+		MinaPublicKey:   MinaPubKey,
+	})
+	require.ErrorIs(t, err, types.ErrInvalidCreatorAddres)
+}
+
+func TestInvalidSigner(t *testing.T) {
+
+	priv := secp256k1.GenPrivKey()
+
+	pub := priv.PubKey()
+
+	secondaryPriv := secp256k1.GenPrivKey()
+
+	secondaryPublic := secondaryPriv.PubKey()
+
+	addr := sdk.AccAddress(secondaryPublic.Address())
+
+	f := initFixture(t)
+	ms := keeper.NewMsgServerImpl(f.keeper)
+
+	_, err := ms.RegisterKeys(f.ctx, &types.MsgRegisterKeys{
+		Creator:         addr.String(),
+		CosmosSignature: mockCosmosSignature,
+		MinaSignature:   mockMinaSignature,
+		CosmosPublicKey: pub.Bytes(),
+		MinaPublicKey:   MinaPubKey,
+	})
+
+	require.ErrorIs(t, err, types.ErrInvalidSigner)
+
+}
+
+// TODO: Update require.NoError to require.ErrorIs once the VerifyCosmosSig and VerifyMinaSig is implemented
+func TestInvalidSignature(t *testing.T) {
+
+	priv := secp256k1.GenPrivKey()
+
+	pub := priv.PubKey()
+
+	addr := sdk.AccAddress(pub.Address())
+
+	f := initFixture(t)
+	ms := keeper.NewMsgServerImpl(f.keeper)
+
+	invalidSig := "cosmosSig"
+
+	_, err := ms.RegisterKeys(f.ctx, &types.MsgRegisterKeys{
+		Creator:         addr.String(),
+		CosmosSignature: invalidSig,
+		MinaSignature:   mockMinaSignature,
+		CosmosPublicKey: pub.Bytes(),
+		MinaPublicKey:   MinaPubKey,
+	})
+
+	require.NoError(t, err)
+
+}
+
 func TestInsertSecondaryKeysFail(t *testing.T) {
 	f := initFixture(t)
 
