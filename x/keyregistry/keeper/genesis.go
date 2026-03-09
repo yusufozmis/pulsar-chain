560ffaf2bbf45550e072dac34b730d76b1b8c0cb
Yusuf <yusufozmis2@gmail.com>
2026-03-09 21:59:17 +0300

    feat: add keypairs to exported genesis file

--- x/keyregistry/keeper/genesis.go
+++ x/keyregistry/keeper/genesis.go
@@ -12,6 +12,7 @@ func (k Keeper) InitGenesis(ctx context.Context, genState types.GenesisState) er
 	keyPairs := genState.KeyPairs
 
 	for _, keyPair := range keyPairs {
+
 		err := k.CosmosToMina.Set(ctx, keyPair.CosmosKey, keyPair.MinaKey)
 		if err != nil {
 			return err
@@ -34,6 +35,63 @@ func (k Keeper) ExportGenesis(ctx context.Context) (*types.GenesisState, error) 
 	if err != nil {
 		return nil, err
 	}
+
+	var keyPairs []*types.KeyPair
+
+	var existenceMap = make(map[string]bool)
+
+	cosmosIterator, err := k.CosmosToMina.Iterate(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer cosmosIterator.Close()
+
+	for cosmosIterator.Valid() {
+		cosmosKey, err := cosmosIterator.Key()
+		if err != nil {
+			return genesis, err
+		}
+		minaKey, err := cosmosIterator.Value()
+		if err != nil {
+			return genesis, err
+		}
+		keyPair := &types.KeyPair{
+			MinaKey:   minaKey,
+			CosmosKey: cosmosKey,
+		}
+		keyPairs = append(keyPairs, keyPair)
+		existenceMap[keyPair.String()] = true
+		cosmosIterator.Next()
+	}
+
+	minaIterator, err := k.MinaToCosmos.Iterate(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer minaIterator.Close()
+
+	for minaIterator.Valid() {
+		minaKey, err := minaIterator.Key()
+		if err != nil {
+			return genesis, err
+		}
+		cosmosKey, err := minaIterator.Value()
+		if err != nil {
+			return genesis, err
+		}
+		keyPair := &types.KeyPair{
+			MinaKey:   minaKey,
+			CosmosKey: cosmosKey,
+		}
+		if existenceMap[keyPair.String()] {
+			minaIterator.Next()
+			continue
+		}
+		keyPairs = append(keyPairs, keyPair)
+		minaIterator.Next()
+	}
+
+	genesis.KeyPairs = keyPairs
 
 	return genesis, nil
 }
