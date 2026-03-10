de84ff47e445fbd52e59a0511d8fb5be03433bd4
Yusuf <yusufozmis2@gmail.com>
2026-03-10 22:33:00 +0300

    test: move keypair tests to new file

--- x/keyregistry/keeper/query_params_test.go
+++ x/keyregistry/keeper/query_params_test.go
@@ -1,14 +1,9 @@
 package keeper_test
 
 import (
-	"crypto/ed25519"
-	"crypto/rand"
 	"testing"
 
-	"github.com/cometbft/cometbft/crypto/secp256k1"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 
 	"github.com/node101-io/pulsar-chain/x/keyregistry/keeper"
 	"github.com/node101-io/pulsar-chain/x/keyregistry/types"
@@ -24,79 +19,4 @@ func TestParamsQuery(t *testing.T) {
 	response, err := qs.Params(f.ctx, &types.QueryParamsRequest{})
 	require.NoError(t, err)
 	require.Equal(t, &types.QueryParamsResponse{Params: params}, response)
-}
-
-// TestCosmosMapInvalidArgumentFail verifies that GetCosmosPubKey returns
-// an InvalidArgument error when called with a nil request.
-func TestCosmosMapInvalidArgumentFail(t *testing.T) {
-	f := initFixture(t)
-
-	qs := keeper.NewQueryServerImpl(f.keeper)
-	params := types.DefaultParams()
-	require.NoError(t, f.keeper.Params.Set(f.ctx, params))
-
-	_, err := qs.GetCosmosPubKey(f.ctx, nil)
-	require.Error(t, err)
-
-	st, _ := status.FromError(err)
-	require.Equal(t, codes.InvalidArgument, st.Code())
-}
-
-// TestMinaMapInvalidArgumentFail verifies that GetMinaPubKey returns
-// an InvalidArgument error when called with a nil request.
-func TestMinaMapInvalidArgumentFail(t *testing.T) {
-	f := initFixture(t)
-
-	qs := keeper.NewQueryServerImpl(f.keeper)
-	params := types.DefaultParams()
-	require.NoError(t, f.keeper.Params.Set(f.ctx, params))
-
-	_, err := qs.GetMinaPubKey(f.ctx, nil)
-	require.Error(t, err)
-
-	st, _ := status.FromError(err)
-	require.Equal(t, codes.InvalidArgument, st.Code())
-}
-
-// TestCosmosMapPubkeyNotFound verifies that GetCosmosPubKey returns
-// a NotFound error when the provided mina public key has no associated cosmos key.
-func TestCosmosMapPubkeyNotFound(t *testing.T) {
-	f := initFixture(t)
-
-	qs := keeper.NewQueryServerImpl(f.keeper)
-	params := types.DefaultParams()
-	require.NoError(t, f.keeper.Params.Set(f.ctx, params))
-
-	pub, _, err := ed25519.GenerateKey(rand.Reader)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = qs.GetCosmosPubKey(f.ctx, &types.QueryGetCosmosPubKeyRequest{
-		MinaPubKey: pub,
-	})
-
-	st, _ := status.FromError(err)
-	require.Equal(t, codes.NotFound, st.Code())
-}
-
-// TestMinaMapPubkeyNotFound verifies that GetMinaPubKey returns
-// a NotFound error when the provided cosmos public key has no associated mina key.
-func TestMinaMapPubkeyNotFound(t *testing.T) {
-	f := initFixture(t)
-
-	qs := keeper.NewQueryServerImpl(f.keeper)
-	params := types.DefaultParams()
-	require.NoError(t, f.keeper.Params.Set(f.ctx, params))
-
-	priv := secp256k1.GenPrivKey()
-
-	pub := priv.PubKey()
-
-	_, err := qs.GetMinaPubKey(f.ctx, &types.QueryGetMinaPubKeyRequest{
-		CosmosPubKey: pub.Bytes(),
-	})
-
-	st, _ := status.FromError(err)
-	require.Equal(t, codes.NotFound, st.Code())
 }
